206357ba0e2c83583e084fb37584c63dbdc0d653
Gcsp1002 <gui.costa1002@gmail.com>
2022-10-05 22:39:53 -0300

    start

--- src/routes/teros.routes.ts
+++ src/routes/teros.routes.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
 
 const terosRoutes = Router();
 
 terosRoutes.post('/csv', (req: Request, res: Response) => {
-  return res.status(200).json({});
+  return res.status(200).send("Hello World");
 });
 
 export { terosRoutes };
