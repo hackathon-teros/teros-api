89c6d22f0fa95daffb5e2e139b2cc4469d7c0d0c
pedrobarretto <pedro@barretto.com.br>
2022-10-05 21:33:17 -0300

    feat: first commit

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
+
+app.post('/teros', (req: Request, res: Response) => {
+  return res.status(200).json({});
+});
 
 app.listen(8080);
