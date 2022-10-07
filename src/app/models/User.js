2c4cacceac9880c8565e70545b9a062ad6e6ea4e
Gustavo Duarte Alves <gustavoduartealves@MacBook-Pro-de-Gustavo.local>
2022-10-07 01:32:50 -0300

    server in root directory

--- src/app/models/User.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const userSchema = new mongoose_1.Schema({
-    email: { type: String, required: true },
-    password: { type: String, required: true }
-});
-exports.default = (0, mongoose_1.model)('users', userSchema);
--- src/env/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongoDatabaseName = exports.mongoUrl = void 0;
-require("dotenv/config");
-_a = process.env, exports.mongoUrl = _a.MONGO_URL, exports.mongoDatabaseName = _a.MONGO_DATABASE_NAME;
